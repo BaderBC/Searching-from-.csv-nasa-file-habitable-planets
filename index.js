3bfefd30a98d2000b668e73feab9b81429ed5876
BaderBC <80683535+BaderBC@users.noreply.github.com>
2022-05-06 00:30:36 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+let http = require('http'),
+    dt = require('./mainModule')
+
+http.createServer(function (req, res){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('My actual time: '+ dt.myDataTime()+'P.S. DZIALA XDDDD!');
+    res.end();
+}).listen(2137);
