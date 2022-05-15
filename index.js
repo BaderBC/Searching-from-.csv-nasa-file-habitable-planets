290d673ae112ca95a88e9af7c7ff054af8c5a56d
BaderBC <niejakibader@gmail.com>
2022-05-15 12:47:56 +0200

    origin

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-let http = require('http');
+const EventEmitter = require('events')
+const {response} = require("express");
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-    res.write('<input type="file" name="filetoupload"><br>');
-    res.write('<input type="submit">');
-    res.write('</form>');
-    return res.end();
-}).listen(8080);
+const BadClup = new  EventEmitter;
+
+
+BadClup.on('status', (listener) => {
+    if(listener === 'udało się'){
+        console.log('To była oczywistość B)')
+    }else if(listener === 'nie udało się'){
+        console.log('Ale jeszcze się uda ;)')
+    }
+})
+
+
+BadClup.emit('status', process.argv.slice(2).join(' '))
+
+//console.log(process.argv.slice(2).join(' '))
+
+
+
+const urls = [
+    'https://jsonplaceholder.typicode.com/users',
+    'https://jsonplaceholder.typicode.com/posts',
+    'https://jsonplaceholder.typicode.com/photos'
+]
+
+
+let promise = Promise.all(urls.map(url => fetch(url).then(result => result.json())));
+promise.then(result => console.log(result))
