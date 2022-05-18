f2d955a62f2cdcddb042c0724d7f226d7b85436b
BaderBC <niejakibader@gmail.com>
2022-05-18 21:04:09 +0200

    origin

---
+++ index.js
@@ -0,0 +1,38 @@
+/*
+This code use data from
+https://www.nasa.gov/mission_pages/kepler/main/index.html
+to search the habitable planets that have been captured by this telescope
+ */
+
+
+const { parse } = require('csv-parse'),
+    fs = require('fs');
+
+
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet){
+    return planet['koi_disposition'] === 'CONFIRMED'
+        && planet['koi_insol'] > 0.36
+        && planet['koi_insol'] < 1.11
+        && planet['koi_prad'] < 1.6;
+}
+
+
+fs.createReadStream('kepler_data.csv')
+    .pipe(parse({
+        comment: "#",
+        columns: true,
+    }))
+    .on('data', (data) => {
+        if (isHabitablePlanet(data)){
+            habitablePlanets.push(data);
+        }
+    })
+    .on('error',(err) => {
+        console.error(err);
+    })
+    .on('end', () => {
+        console.log(habitablePlanets.map(planet => planet['kepler_name']))
+        console.log(`${habitablePlanets.length} habitable planets found ;)`);
+    })
